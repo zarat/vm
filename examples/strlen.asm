4ce854e4b51c9ce556f038bf521f4ebde76b6137
Manuel Zarat <manuel@zarat.ml>
2021-11-29 22:48:18 +0100

    Add files via upload

--- examples/strlen.asm
+++ examples/strlen.asm
@@ -1,24 +1,29 @@
 ; count the length of a string using memory and using the stack.
 
+; read a string from stdin and store it at memory location 1
 push 1
 read
 
-push 1 ; address
+; strlen_memory will compute the length and store it in ax
+push 1 ; memory location of the string
 call strlen_memory
 
-ldr r2
+; print the length stored in ax
+ldr ax
 print
+
+; print a line feed
 push 10
 printc
- 
-push 0 ; terminator
-push 1 ; address
-gets 
 
-mov r1 0 ; counter for strlen    
+; strlen_stack which will count the elements on the stack until it finds a 0 and store it in ax
+push 0 ; null terminator to mark the end
+push 1 ; memory location
+gets 
+mov r1 0 ; set the counter for strlen to 0   
 call strlen_stack
 
-ldr r1 ; print the length stored at ax
+ldr ax ; print the length stored in ax
 print
 
 jmp end
@@ -32,7 +37,7 @@ strlen_memory:
     call strlen_memory_loop
     ret
 strlen_memory_loop:
-    ldr r1 ; address
+    ldr r1 ; location
     ldr r2 ; index (counter)
     ldm
     push 0
@@ -41,6 +46,7 @@ strlen_memory_loop:
     inc r2
     jmp strlen_memory_loop
 strlen_memory_end:
+    mov ax r2 ; store result in ax
     ret
 
 ; get length of null terminated data on the stack   
@@ -50,6 +56,7 @@ strlen_stack:
     push 0 ; 0 to compare with the next char            
     eq 
     jnz strlen_stack_loop
+    mov ax r1
     ret           
     
 end:
