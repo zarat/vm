c603cd7c53ea585956382b0a948588893b129fbb
Manuel Zarat <manuel@zarat.ml>
2021-12-07 15:24:13 +0100

    Add files via upload

--- examples/string.asm
+++ examples/string.asm
@@ -84,6 +84,54 @@ ldr bx
 push 'd'
 print
 
+push 10 
+printc
+
+; ston
+int 9 ; ARITH_CHAR
+push "Type a number: "
+si ax
+ldr ax
+push 1
+puts
+push 1
+write
+
+int 10 ; ARITH_INT
+push 1 
+read
+; get the length of the input
+push 1
+gets
+si ax
+dec ax ; input from read is \x00 terminated, remove it
+ldr ax
+push 1
+puts
+
+push 10 ; base
+ldr ax ; no of digits
+push 1  ; addr of string
+call string_to_number ; result is at ax
+mov r1 ax ; store in r1
+
+int 9 ; ARITH_CHAR
+push "Your input x 2: "
+si ax
+dec ax ; remove \00
+ldr ax
+push 1
+puts
+push 1
+write
+int 10 ; ARITH_INT
+
+; print result at ax 
+mul r1 2
+ldr r1
+push 'd'
+print
+
 jmp end
 
 ; strpos
@@ -187,6 +235,34 @@ strlen_stack:
     eq 
     jnz strlen_stack_loop
     mov ax r1
-    ret     
+    ret 
+
+; convert a string into a number    
+string_to_number:
+    pop r1 ; address of string
+    pop r2 ; no of digits
+    dec r2
+    pop r3 ;mov r3 10 ; base
+    mov r4 0 ; final result 
+    mov r5 1 ; current index
+    call string_to_number_loop 
+    mov ax r4   
+    ret        
+string_to_number_loop_step:
+    push 1 
+    ldr r2 
+    ldm    
+    pop r6        
+    sub r6 48 ; from ascii
+    mul r6 r5 ; multiply currenet index by multiplier
+    add r4 r6        
+    mul r5 r3 ; mul the multiplier by the base every loop
+    dec r2 ; decrease digit counter        
+string_to_number_loop:
+    push 0
+    ldr r2 ; digit counter == 0 -> done
+    geq
+    jz string_to_number_loop_step
+    ret    
    
 end:    
