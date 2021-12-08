1d14436fa37ff351300c757d671483ff07b6d9e3
Manuel Zarat <manuel@zarat.ml>
2021-12-08 05:53:04 +0100

    Update arith.asm

--- examples/arith.asm
+++ examples/arith.asm
@@ -9,39 +9,25 @@ jmp end
 
 chartest:    
     int 9 ; ARITH_CHAR
-    ; mov is not affected by arith mode
-    mov r7 31415
-    mov r8 10000
-    mov r9 0
-    ; add is affected, 3.1415 which dows not fit into a single byte
-    add r9 r7
-    div r9 r8
-    ; print
-    ldr r9 push 'c' print push 10 printc
+    mov r7 65
+    add r7 1
+    ldr r7 push 'c' print push 10 printc
     ret
 
 inttest:
     int 10 ; ARITH_INT
     mov r7 31415
     mov r8 10000
-    mov r9 0
-    ; div is affected, 3.14 dows fit into 4 bytes but is stored in a different way then integers
-    ; so we end up with just the integer value before the decimal point
-    add r9 r7
-    div r9 r8 
-    ; print
-    ldr r9 push 'd' print push 10 printc
+    div r7 r8 
+    ldr r7 push 'd' print push 10 printc
     ret
 
 floattest:
     int 11 ; ARITH_FLOAT 
     mov r7 31415
     mov r8 10000
-    mov r9 0
-    add r9 r7
-    div r9 r8
-    ; print
-    ldr r9 push 'f' print
+    div r7 r8
+    ldr r7 push 'f' print
     ret
  
 end:
